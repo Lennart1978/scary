7bb315ef6bf6a56b4ff929e7c19e04293861ead1
Lennart <monkeynator78@gmail.com>
2024-07-18 22:28:05 +0200

    Add files via upload

---
+++ esrever.c
@@ -0,0 +1,95 @@
+
+}
+;0 nruter    
+;)]2[vgra ,"n\s% ot nettirw dna desrever yllufsseccus eliF"(ftnirp    
+
+;)reffub(eerf    
+;)elif_tuptuo(esolcf    
+
+}    
+;1 nruter        
+;)elif_tuptuo(esolcf        
+;)reffub(eerf        
+;)"elif tuptuo gnitirw rorrE"(rorrep        
+{ )ezis_elif =! ezis_etirw( fi    
+;)elif_tuptuo ,ezis_elif ,1 ,reffub(etirwf = ezis_etirw t_ezis    
+
+}    
+;1 nruter        
+;)reffub(eerf        
+;)"elif tuptuo gninepo rorrE"(rorrep        
+{ )LLUN == elif_tuptuo( fi    
+;)"bw" ,]2[vgra(nepof = elif_tuptuo* ELIF    
+
+;)ezis_elif ,reffub(8ftu_esrever    
+
+;)elif_tupni(esolcf    
+
+}    
+;1 nruter        
+;)elif_tupni(esolcf        
+;)reffub(eerf        
+;)"elif tupni gnidaer rorrE"(rorrep        
+{ )ezis_elif =! ezis_daer( fi    
+;)elif_tupni ,ezis_elif ,1 ,reffub(daerf = ezis_daer t_ezis    
+
+}    
+;1 nruter        
+;)elif_tupni(esolcf        
+;)"rorre noitacolla yromeM"(rorrep        
+{ )LLUN == reffub( fi    
+;)ezis_elif(collam = reffub* rahc    
+
+;)elif_tupni(dniwer    
+;)elif_tupni(lletf = ezis_elif gnol    
+;)DNE_KEES ,0 ,elif_tupni(keesf    
+
+}    
+;1 nruter        
+;)"elif tupni gninepo rorrE"(rorrep        
+{ )LLUN == elif_tupni( fi    
+;)"br" ,]1[vgra(nepof = elif_tupni* ELIF    
+
+;)"" ,LLA_CL(elacoltes    
+
+}    
+;1 nruter        
+;)]0[vgra ,"n\>elif_tuptuo< >elif_tupni< s% :egasU" ,rredts(ftnirpf        
+{ )3 =! cgra( fi    
+{ )][vgra* rahc ,cgra tni(niam tni
+
+}
+;)desrever(eerf    
+;)ezis ,desrever ,reffub(ypcmem    
+
+}    
+;nel =+ sop        
+}        
+;]i + sop[reffub = ]i + nel - sop - ezis[desrever            
+{ )++i ;nel < i ;0 = i t_ezis( rof        
+
+}        
+;4 = nel            
+{ )0Fx0 == )8Fx0 & ]sop[reffub(( fi esle }        
+;3 = nel            
+{ )0Ex0 == )0Fx0 & ]sop[reffub(( fi esle }        
+;2 = nel            
+{ )0Cx0 == )0Ex0 & ]sop[reffub(( fi esle }        
+;1 = nel            
+{ )0 == )08x0 & ]sop[reffub(( fi        
+;1 = nel t_ezis        
+{ )ezis < sop( elihw    
+;0 = sop t_ezis    
+
+}    
+;nruter        
+;)"rorre noitacolla yromeM"(rorrep        
+{ )LLUN == desrever( fi    
+;)ezis(collam = desrever* rahc    
+{ )ezis t_ezis ,reffub* rahc(8ftu_esrever diov
+
+.reffub nevig eht ni sretcarahc dedocne 8-FTU fo redro eht sesreveR //
+>h.elacol< edulcni#
+>h.gnirts< edulcni#
+>h.bildts< edulcni#
+>h.oidts< edulcni#
